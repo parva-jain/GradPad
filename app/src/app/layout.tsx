16e525a9f52adcac7ea1ad7fa50db3ac5664cb0a
Parva Jain <parvajainpjjp@gmail.com>
2026-06-06 09:28:08 +0530

    fix: reconcile background token, add suppressHydrationWarning, move shadcn to devDeps

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${plusJakartaSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
